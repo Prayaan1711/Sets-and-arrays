c49343c84448673395456b4aeb40fd711a744482
Prayaan1711 <spyfyi1711@gmail.com>
2025-06-21 09:38:43 +0530

    1234

---
+++ stesop.py
@@ -0,0 +1,18 @@
+myset = {1,2,3}
+print(myset)
+
+myset = {1.0,"hello",(1,2,3)}
+print(myset)
+
+myset = {1,2,3,4,3,2}
+print(myset)
+
+myset = set([1,2,3,2])
+print(myset, "\n")
+
+numset = {0,1,3,4,5}
+print("original set:")
+print(numset)
+
+numset.pop()
+print(numset, "\n")
