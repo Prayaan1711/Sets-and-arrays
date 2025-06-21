4b5b93a8c30940af21cae32bfca4f1c62dffbc4b
Prayaan1711 <spyfyi1711@gmail.com>
2025-06-21 09:44:55 +0530

    1234

---
+++ intset.py
@@ -0,0 +1,10 @@
+setx = {"green", "blue"}
+sety = {"blue", "black"}
+
+print("original set elements: ")
+print(setx)
+print(sety)
+
+print("Intersection: ")
+setz = setx.intersection(sety)
+print(setz)
